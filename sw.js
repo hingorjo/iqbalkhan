e4ed98120291c8c6c9c215d2d205f7959e8cbf83
Iqbal khan <hingorjoiqbal123@gmail.com>
2026-01-19 18:19:25 +0500

    feat: Implement PWA features with a service worker and web app manifest, and add new styling for the skills section and a coming soon container.

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'portfolio-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './me.jpeg',
+    './favicon.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://unpkg.com/aos@2.3.1/dist/aos.css',
+    'https://unpkg.com/aos@2.3.1/dist/aos.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
